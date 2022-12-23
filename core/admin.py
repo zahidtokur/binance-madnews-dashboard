bd2caba7bf50a7c25d741ccd11cf1a26f3d13406
Zahid Tokur <zahidtokur@gmail.com>
2022-12-23 11:58:54 +0300

    Add core models to admin site

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from core.models import Account, Pair
 
-# Register your models here.
+
+class AccountAdmin(admin.ModelAdmin):
+    pass
+
+
+class PairAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Account, AccountAdmin)
+admin.site.register(Pair, PairAdmin)
